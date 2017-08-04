b7f94bf550fcc35a567036c71e2cfc7848643b30
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-08-04 14:24:00 -0400

    added stuff

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
-class PostsController < ApplicationRecord
+class PostsController < ApplicationController
 
-  
+  def new
+    @prompt = Prompt.find(params[:prompt_id])
+    @post = @prompt.posts.new
+  end
+
+  def create
+    @prompt = Prompt.find(params[:prompt_id])
+    @post = @prompt.posts.create(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
