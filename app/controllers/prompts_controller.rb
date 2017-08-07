36dcd3cb32058b767320dc637e01d425f3260c96
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-08-07 14:47:08 -0400

    anchored footer

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -9,4 +9,19 @@ class PromptsController < ApplicationController
     @random_prompt = Prompt.all.sample
   end
 
+  def new
+    @prompt = Prompt.new
+  end
+
+  def create
+    @prompt = Prompt.create(prompt_params)
+    redirect_to prompts_path
+  end
+
+
+  private
+  def prompt_params
+    params.require(:prompt).permit(:title, :prompt, :img_url)
+  end
+
 end
